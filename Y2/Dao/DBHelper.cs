638c84f7d463c725315489809276bc7d9362c48b
NaiS <2432048988@qq.com>
2019-06-13 16:49:34 +0800

    七层架构

---
+++ Y2/Dao/DBHelper.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dao
+{
+   public  class DBHelper
+    {
+        private static SqlConnection GetCon()
+        {
+            SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=Test;Persist Security Info=True;User ID=sa;Password=root");
+            return con;
+        }
+        public static DataTable Select(string sql,string dname,params DbParameter[] ps)
+        {
+            SqlConnection con = GetCon();
+            SqlDataAdapter da = new SqlDataAdapter(sql, con);
+            if (ps!=null) {
+                da.SelectCommand.Parameters.AddRange(ps);
+
+            }
+            DataSet dt = new DataSet();
+            try
+            {
+                da.Fill(dt,dname);
+            }
+            catch (Exception ex)
+            {
+                xError( ex);
+                // throw;
+            }
+            return dt.Tables[0];
+        
+
+
+        }
+
+        public static int InsertUpdateDel(string sql,params DbParameter[] ps) {
+            SqlConnection con = GetCon();
+            SqlCommand com = new SqlCommand(sql,con);
+            if (ps!=null) {
+
+                com.Parameters.AddRange(ps);
+            }
+            int i = 0;
+            try
+            {
+                con.Open();
+                i = com.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                xError( ex);
+                //throw;
+            }
+            finally {
+
+
+                con.Close();
+            }
+            return i;
+
+        }
+
+
+        public static object SelectSinger(string sql, params DbParameter[] ps) {
+            SqlConnection con = GetCon();
+            SqlCommand com = new SqlCommand(sql, con);
+            if (ps != null)
+            {
+
+                com.Parameters.AddRange(ps);
+            }
+            object obj = null;
+            try
+            {
+              con.Open();
+                obj = com.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                xError( ex);
+                //throw;
+            }
+            finally
+            {
+
+          con.Close();
+            }
+            return obj;
+
+
+        }
+
+
+        public static SqlDataReader SelectReader(string sql, params DbParameter[] ps) {
+            SqlConnection con = GetCon();
+            SqlCommand com = new SqlCommand(sql,con);
+            if(ps!=null){
+                com.Parameters.AddRange(ps);
+            }
+            SqlDataReader reader = null;
+            try
+            {
+                con.Open();
+                reader = com.ExecuteReader(CommandBehavior.CloseConnection);
+            }
+            catch (Exception ex)
+            {
+                xError( ex);
+                //throw;
+            }
+            return reader;
+        }
+
+        public static DataTable SelectCunCu(string sql,params DbParameter[] ps) {
+            SqlConnection con = GetCon();
+            SqlDataAdapter da = new SqlDataAdapter(sql,con);
+            if (ps!=null) {
+                da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                da.SelectCommand.Parameters.AddRange(ps);
+            }
+            DataTable dt = new DataTable();
+            try
+            {
+                da.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                xError(ex);
+                //throw;
+            }
+            return dt;
+        }
+
+
+        public static void xError(Exception ex) {
+            using (StreamWriter sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory+ "/Error.txt",true)) {
+
+                sw.WriteLine("错误信息：" + ex.Message);
+                sw.WriteLine("错误时间:" + DateTime.Now); 
+                sw.WriteLine("----------------------------");
+
+            }
+
+        }
+    }
+}
---
+++ Y2/IocContianer/iocCreate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Unity;
+using Dao;
+using System.Configuration;
+using Microsoft.Practices.Unity.Configuration;
+
+namespace IocCreate
+{
+   public  class iocCreate
+    {
+
+        //public static Itext01Dao<text01>  text01Dao()    {
+
+        //    UnityContainer ioc = new UnityContainer();
+        //    ioc.RegisterType<Itext01Dao<text01>, text01Dao>();
+        //    return ioc.Resolve<Itext01Dao<text01>>();
+
+        //}
+       
+       // public static Itext01BLL CreateTextBll()
+        //{
+            
+        //    UnityContainer ioc = new UnityContainer();
+        //    ExeConfigurationFileMap ef = new ExeConfigurationFileMap();
+
+        //    ef.ExeConfigFilename = AppDomain.CurrentDomain.BaseDirectory+"Unity.config";
+
+        //    Configuration cf = ConfigurationManager.OpenMappedExeConfiguration(ef, ConfigurationUserLevel.None);
+
+        //    UnityConfigurationSection uc = (UnityConfigurationSection)cf.GetSection("unity");
+
+        //    ioc.LoadConfiguration(uc, "containerTow");
+
+        //    return ioc.Resolve<Itext01BLL>("text01BLL");
+        //    //ioc.RegisterType<IBookBLL,>
+        //}
+    }
+}
