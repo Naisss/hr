311cd413e04973402d021628da912e9c643408a6
Administrator <Administrator@KSL1YD8G6K86MUZ>
2019-06-14 15:02:19 +0800

    DaoBase

---
+++ Y2/IBLL/Ifirst_kindBLL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+namespace IBLL
+{
+  public  interface Ifirst_kindBLL
+    {
+        List<config_file_first_kind> SelectAll();
+    }
+}
---
+++ Y2/IDao/Ifirst_kindDao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+namespace IDao
+{
+   public interface Ifirst_kindDao
+    {
+        List<config_file_first_kind> SelectAll();
+
+    }
+}
